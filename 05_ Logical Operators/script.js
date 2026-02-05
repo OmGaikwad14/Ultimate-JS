d235a896ed92f4659aef26deffed813e42dcd86d
Om Gaikwad <omgaikwad140@gmail.com>
2026-02-05 12:45:22 +0530

    Implement logical operators examples in script.js
    Added examples of logical operators including AND, OR, and NOT.

--- 05_ Logical Operators/script.js
+++ 05_ Logical Operators/script.js
@@ -1,1 +1,23 @@
+// Logical Operators
+// AND -> All cond. must be true (&&)
 
+const age = 20;
+const gender = 'female';
+
+if (age >= 18 && gender == 'male') {
+    console.log('You are adult male');
+}
+
+// OR -> Atleast one cond. should be true (||)
+
+if (age >= 18 || gender == 'male') {
+    console.log('You are adult male');
+}
+
+// NOT -> True:False & False:True (!)
+
+const number = 5;
+
+if (!(number % 2 == 0)) {
+    console.log('Odd');
+}
